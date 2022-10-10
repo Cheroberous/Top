3b9b83a0a9040a7927519354ca0ee34938dada40
Cheroberous <1895042@studenti.uniroma1.it>
2022-10-10 23:59:21 +0200

    sistemate due funzioni + appunti sul da fare

--- p_manager.c
+++ p_manager.c
@@ -11,9 +11,13 @@
 //
 #define QUIT_COMMAND  "QUIT"
 
+char** alloca_m(int n);
+int popola_ris(char** m,int n,struct dirent** files_1);
+
 int main(int argc,char* argv[]){
 
     // ORGANIZZO VARIABILI
+    struct dirent **files;
     int r;
     char** file_in_proc;
     char *quit_command = QUIT_COMMAND;
@@ -48,42 +52,92 @@ int main(int argc,char* argv[]){
 
 
        
-        //printf("==> Inizio richiesta informazioni\n");
+   
         printf("eseguo comando == %s\n",richiesta);
         
         //----------------------------------------------------------------------------------------
         // getcwd per la directory corrente
 
-        struct dirent **files;
-        int n = scandir("/proc", &files, NULL, alphasort);
-        //int n = scandir("/home/paolascema/Scrivania/", &files, NULL, alphasort);
-
-        // alloco 
-        file_in_proc = malloc(n*sizeof(char*));
-        for (int r=0;r < n;r++){
-            *(file_in_proc+r) = malloc(20*sizeof(char));    // 20 abbastanza arbitrario (dovresti aggiustare)
-        }
-
-
-        //controllo risultati
+      
+        
+        int n = scandir("/proc", &files, NULL, alphasort); // metto risultato scandir nella struct files
 
         if (n < 0) {
             printf("errore richiesta file inp proc");
             exit(EXIT_FAILURE);
         }
+ 
 
-        /* vedo cosa trovo */
+        // alloco matrice per nomi
+
+        file_in_proc=alloca_m(n);      // n
+
+        // filtro risultati
+
+        int quanti=popola_ris(file_in_proc,n,files);
+
+        printf("stampo file salvati in proc (quelli numerici)\n");
+        
+        for (int j=0;j<quanti;j++){
+
+            printf("file == > %s\n", file_in_proc[j]);
+            
+
+        }
+
+        printf("FINE STAMPA\n");
+
+        // free 
         for (int i = 0; i < n; i++) {
+            free(files[i]);
+        }
+        free(files);
+        printf("DEALLOCATO\n");
+
+
+
+        //printf("Files in Directory are: \n");
+        //execl("/bin/ls","/home/paolascema/Scrivania/ls","-l",0);    // funge
+        //deve essere eseguitada un thread/processo altrimenti mi fa terminare il programma
+        // non posso cambiare la directory
+
+    }
+
+
+
+
+    printf("entrato comando quit \n");
+    fflush(stdout);
+    
+
+
+}
+
+
+char** alloca_m(int n){
+    char** m;
+    m = malloc(n * sizeof(char *));
+    for (int r = 0; r < n; r++){
+            
+        *(m + r) = malloc(20 * sizeof(char)); // 20 abbastanza arbitrario (dovresti aggiustare)
+    }
+    
+    return m;
+
+}
+
+int popola_ris(char** m,int n, struct dirent** files_1){
+    int k=0;
+
+    for (int i = 0; i < n; i++) {
 
             /* associo risultato a struttura  ( guarda altre voci )*/
 
-            struct dirent *file_dir = files[i];
-            file_in_proc[i]=file_dir->d_name;
-
+            struct dirent *file_dir = files_1[i];
 
             // fai funzione apposita magari dove settare regex
             regex_t regex;
-            r = regcomp(&regex, "[0-9]", 0);
+            int r = regcomp(&regex, "[0-9]", 0);
             if (r) {
                 fprintf(stderr, "Could not compile regex\n");
                 exit(1);
@@ -91,9 +145,12 @@ int main(int argc,char* argv[]){
 
 
             r = regexec(&regex, file_dir->d_name, 0, NULL, 0);
+
                 if( !r ){
                         //puts("Match");
-                        printf("%s\n", file_in_proc[i]);
+                        m[k] = file_dir->d_name;
+                        //printf("nella fnz %s\n", m[k]);
+                        k++;
                 }
                 else if( r == REG_NOMATCH ){
                         //puts("No match");
@@ -106,36 +163,8 @@ int main(int argc,char* argv[]){
 
             regfree(&regex);
 
-
-           
-
-         
         }
 
-        /* libero mem (fare funzione a parte) */
-        for (int i = 0; i < n; i++) {
-            free(files[i]);
-        }
-        free(files);
-
-    
-
-
-        //printf("Files in Directory are: \n");
-        //execl("/bin/ls","/home/paolascema/Scrivania/ls","-l",0);    // funge
-        //deve essere eseguitada un thread/processo altrimenti mi fa terminare il programma
-        // non posso cambiare la directory
-
-        
-
-    }
-
-
-
-
-    printf("entrato comando quit \n");
-    fflush(stdout);
-    
-
+       return k;
 
 }
